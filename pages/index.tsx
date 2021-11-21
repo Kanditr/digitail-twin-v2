04becaf07a3f64ba084b7728b746baab66a559aa
Kanditr <gun.rodwong@gmail.com>
2021-11-21 17:42:24 +0700

    bugfix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import type { NextPage } from "next";
 import React from "react";
-import Modal from "../components/Modal/modal";
+// import Modal from "../components/Modal/modal";
 import Hero from "./Home/Hero/hero";
 import Selection from "./Home/Selection/index";
 import Popular from "./Home/Popular/popular";
-import HotBid from "../components/HotBid/index";
-import Collections from "./Home/Collections/index";
+// import HotBid from "../components/HotBid/index";
+// import Collections from "./Home/Collections/index";
 import Discover from "./Home/Discover/discover";
-import Description from "./Home/Description/index";
+// import Description from "./Home/Description/index";
 import Header from "../components/Header/header";
 import Footer from "../components/Footer/footer";
 
