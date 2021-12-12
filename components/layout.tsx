c104922a0be96c06234a389dfa325d81ce80d86e
Kanditr <gun.rodwong@gmail.com>
2021-12-12 13:27:14 +0700

    DT-100-profile

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,51 +1,10 @@
 import Footers from "./Footer/footer";
 import Headers from "./Header/header";
-import User from "./Header/User/user";
-import { doc, getDoc } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { db } from "../firebase";
-import { useWeb3React } from "@web3-react/core";
 
 export default function Layout({ children }: any) {
-  const [single, setSingle] = useState({}) as any;
-  const [loadAccount, setLoadAccount] = useState(true);
-
-  const { active, account, library, connector, activate, deactivate }: any =
-    useWeb3React();
-
-  // useEffect(() => {
-  //   // load account
-  //   getSingle();
-  // }, []);
-
-  const getSingle = async () => {
-    if (active === true) {
-      const docRef = doc(db, "users", account);
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        console.log("Have Document data", docSnap.data());
-      } else {
-        console.log("No such document!");
-      }
-
-      setSingle(docSnap.data());
-    } else {
-      console.log("No connection");
-    }
-  };
-
-  // load account
-  if (loadAccount === true) {
-    if (active === true) {
-      getSingle();
-      setLoadAccount(false);
-    }
-  }
-
   return (
     <div>
-      <Headers single={single} />
+      <Headers />
       {children}
       <Footers />
     </div>
